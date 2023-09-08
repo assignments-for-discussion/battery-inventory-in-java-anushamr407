e55c5c09f5e08a6e8dc74680e3b4bc7d25384923
Anushamr407 <96069017+anushamr407@users.noreply.github.com>
2023-09-08 20:25:41 +0530

    Update Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -1,45 +1,48 @@
-import java.util.HashMap;
-import java.util.Map;
+package bunchbysoh;
 
-public class BatteryClassification {
-    public static void main(String[] args) {
-        double ratedCapacity = 120.0; // Rated capacity of all batteries (in Ah)
-        double[] presentCapacities = {105.0, 95.0, 60.0, 110.0, 75.0, 100.0};
-
-        Map<String, Integer> classificationCounts = countBatteryClassification(presentCapacities, ratedCapacity);
-
-        System.out.println("Classification Counts:");
-        for (Map.Entry<String, Integer> entry : classificationCounts.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
+public class Main {
+    static class CountsBySoH {
+        public int healthy = 0;
+        public int exchange = 0;
+        public int failed = 0;
     }
 
-    public static Map<String, Integer> countBatteryClassification(double[] presentCapacities, double ratedCapacity) {
-        Map<String, Integer> classifications = new HashMap<>();
-        classifications.put("Healthy", 0);
-        classifications.put("Replace", 0);
-        classifications.put("Failed", 0);
+    static CountsBySoH countBatteriesByHealth(int[] presentCapacities) {
+        CountsBySoH counts = new CountsBySoH();
 
-        for (double presentCapacity : presentCapacities) {
-            double soh = calculateSoH(presentCapacity, ratedCapacity);
-            String classification = classifyBattery(soh);
-            classifications.put(classification, classifications.get(classification) + 1);
+        for (int capacity : presentCapacities) {
+            double soh = calculateSoH(capacity, 120); // Assuming rated capacity is 120 Ah
+            classifyBattery(counts, soh);
         }
 
-        return classifications;
+        return counts;
     }
 
-    public static double calculateSoH(double presentCapacity, double ratedCapacity) {
-        return (presentCapacity / ratedCapacity) * 100.0;
+    static double calculateSoH(int presentCapacity, int ratedCapacity) {
+        return (double) presentCapacity / ratedCapacity * 100.0;
     }
 
-    public static String classifyBattery(double soh) {
+    static void classifyBattery(CountsBySoH counts, double soh) {
         if (soh > 80.0) {
-            return "Healthy";
+            counts.healthy++;
         } else if (soh >= 65.0) {
-            return "Replace";
+            counts.exchange++;
         } else {
-            return "Failed";
+            counts.failed++;
         }
+    }
+
+    static void testBucketingByHealth() {
+        System.out.println("Counting batteries by SoH...\n");
+        int[] presentCapacities = {115, 118, 80, 95, 91, 77};
+        CountsBySoH counts = countBatteriesByHealth(presentCapacities);
+        assert(counts.healthy == 2);
+        assert(counts.exchange == 3);
+        assert(counts.failed == 1);
+        System.out.println("Done counting :)\n");
+    }
+
+    public static void main(String[] args) {
+        testBucketingByHealth();
     }
 }
